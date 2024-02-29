6bccf94dbe232836cdc2d661fcc330772e4a92f6
Sami J. Mäkinen <sjm@iki.fi>
2024-02-29 23:42:51 +0200

    Refactoring

--- src/clock.rs
+++ src/clock.rs
@@ -4,14 +4,13 @@ use crate::*;
 
 use chrono::*;
 use esp_idf_svc::sntp;
-use std::rc::Rc;
 use tokio::time::{sleep, Duration};
 
 const DEFAULT_VSCROLLD: u8 = 20;
 const CONFIG_RESET_COUNT: i32 = 9;
 
 // #[allow(unused_variables)]
-pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Result<()> {
+pub async fn run_clock(mut state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Result<()> {
     // set up SPI bus and MAX7219 driver
 
     let pins = state.pins.write().await.take().unwrap();
@@ -26,17 +25,14 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
     )?;
     let spiconfig = spi::config::Config::new().baudrate(10.MHz().into());
     let spi_dev = spi::SpiDeviceDriver::new(spi_driver, Some(pins.cs), &spiconfig)?;
-    let led_mat = Rc::new(RwLock::new(MAX7219::from_spi(8, spi_dev).unwrap()));
+    let mut led_mat = MAX7219::from_spi(8, spi_dev).unwrap();
 
     // set up led matrix display
 
-    {
-        let mut mat = led_mat.write().await;
-        mat.power_on().ok();
-        for i in 0..8 {
-            mat.clear_display(i).ok();
-            mat.set_intensity(i, 1).ok();
-        }
+    led_mat.power_on().ok();
+    for i in 0..8 {
+        led_mat.clear_display(i).ok();
+        led_mat.set_intensity(i, 1).ok();
     }
     let mut disp = MyDisplay::new_upside_down();
 
@@ -47,34 +43,26 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
             break;
         }
         disp.print(&format!("WiFi ({})", SPIN[cnt % 4]), false);
-        disp.show(&mut *led_mat.write().await);
+        disp.show(&mut led_mat);
 
         if button.is_low() {
-            Box::pin(reset_button(state.clone(), &button, led_mat.clone())).await?;
+            Box::pin(reset_button(&mut state, &button, &mut led_mat)).await?;
         }
 
         cnt += 1;
         sleep(Duration::from_millis(200)).await;
     }
 
-    Box::pin(disp.vscroll(
-        DEFAULT_VSCROLLD,
-        false,
-        &mut *led_mat.write().await,
-        "Connect!",
-    ))
-    .await;
+    Box::pin(disp.vscroll(DEFAULT_VSCROLLD, false, &mut led_mat, "Connect!")).await;
     sleep(Duration::from_millis(1000)).await;
 
     // show our IP address briefly
 
     let ip_info = format!("IP: {}", state.ip_addr.read().await);
-    {
-        let mut mat = led_mat.write().await;
-        Box::pin(disp.vscroll(DEFAULT_VSCROLLD, true, &mut mat, &ip_info)).await;
-        sleep(Duration::from_millis(500)).await;
-        Box::pin(disp.marquee(15, &mut mat, &ip_info)).await;
-    }
+
+    Box::pin(disp.vscroll(DEFAULT_VSCROLLD, true, &mut led_mat, &ip_info)).await;
+    sleep(Duration::from_millis(500)).await;
+    Box::pin(disp.marquee(15, &mut led_mat, &ip_info)).await;
 
     // start up NTP
     let ntp = sntp::EspSntp::new_default()?;
@@ -86,29 +74,23 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
         }
 
         disp.print(&format!("NTP..({})", SPIN[cnt % 4]), false);
-        disp.show(&mut *led_mat.write().await);
+        disp.show(&mut led_mat);
 
         if button.is_low() {
-            Box::pin(reset_button(state.clone(), &button, led_mat.clone())).await?;
+            Box::pin(reset_button(&mut state, &button, &mut led_mat)).await?;
         }
 
         cnt += 1;
         sleep(Duration::from_millis(200)).await;
     }
 
-    Box::pin(disp.vscroll(
-        DEFAULT_VSCROLLD,
-        true,
-        &mut *led_mat.write().await,
-        "NTP OK! ",
-    ))
-    .await;
+    Box::pin(disp.vscroll(DEFAULT_VSCROLLD, true, &mut led_mat, "NTP OK! ")).await;
     sleep(Duration::from_millis(500)).await;
 
     // set up language and timezone
 
-    let lang = &state.config.read().await.lang;
-    let tz = &*state.tz.read().await;
+    let lang = state.config.read().await.lang.to_owned();
+    let tz = state.tz.read().await.to_owned();
 
     // finally, move to the main clock display loop
 
@@ -128,10 +110,10 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
         }
 
         if button.is_low() {
-            Box::pin(reset_button(state.clone(), &button, led_mat.clone())).await?;
+            Box::pin(reset_button(&mut state, &button, &mut led_mat)).await?;
         }
 
-        let local = Utc::now().with_timezone(tz);
+        let local = Utc::now().with_timezone(&tz);
         let sec = local.second();
         let min = local.minute();
         let hour = local.hour();
@@ -150,13 +132,13 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
         if let Some(dir) = time_vscroll {
             let intensity = if (0..=7).contains(&hour) { 1 } else { 8 };
             for i in 0..8 {
-                led_mat.write().await.set_intensity(i, intensity).ok();
+                led_mat.set_intensity(i, intensity).ok();
             }
 
-            Box::pin(disp.vscroll(DEFAULT_VSCROLLD, dir, &mut *led_mat.write().await, &ts)).await;
+            Box::pin(disp.vscroll(DEFAULT_VSCROLLD, dir, &mut led_mat, &ts)).await;
         } else {
             disp.print(&ts, false);
-            disp.show(&mut *led_mat.write().await);
+            disp.show(&mut led_mat);
         }
 
         time_vscroll = match sec {
@@ -175,31 +157,13 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
                 let date_s1 = format!("{wday_s} {day}. ");
                 let date_s2 = format!("{mon_s} {year}  ");
 
-                Box::pin(disp.vscroll(
-                    DEFAULT_VSCROLLD,
-                    true,
-                    &mut *led_mat.write().await,
-                    &date_s1,
-                ))
-                .await;
+                Box::pin(disp.vscroll(DEFAULT_VSCROLLD, true, &mut led_mat, &date_s1)).await;
                 sleep(Duration::from_millis(1500)).await;
 
-                Box::pin(disp.vscroll(
-                    DEFAULT_VSCROLLD,
-                    true,
-                    &mut *led_mat.write().await,
-                    &date_s2,
-                ))
-                .await;
+                Box::pin(disp.vscroll(DEFAULT_VSCROLLD, true, &mut led_mat, &date_s2)).await;
                 sleep(Duration::from_millis(1500)).await;
 
-                Box::pin(disp.vscroll(
-                    DEFAULT_VSCROLLD,
-                    false,
-                    &mut *led_mat.write().await,
-                    &date_s1,
-                ))
-                .await;
+                Box::pin(disp.vscroll(DEFAULT_VSCROLLD, false, &mut led_mat, &date_s1)).await;
                 Some(false)
             }
 
@@ -218,13 +182,8 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
                         // OK we have the temp reading, show it.
 
                         let temp_s = format!("{t:+.1}°C");
-                        Box::pin(disp.vscroll(
-                            DEFAULT_VSCROLLD,
-                            false,
-                            &mut *led_mat.write().await,
-                            &temp_s,
-                        ))
-                        .await;
+                        Box::pin(disp.vscroll(DEFAULT_VSCROLLD, false, &mut led_mat, &temp_s))
+                            .await;
                         sleep(Duration::from_millis(1500)).await;
 
                         Some(true)
@@ -238,16 +197,16 @@ pub async fn run_clock(state: Arc<std::pin::Pin<Box<MyState>>>) -> anyhow::Resul
 
         // Whoa, we have an incoming message to display!
         if let Some(msg) = state.msg.write().await.take() {
-            Box::pin(disp.message(DEFAULT_VSCROLLD, &mut *led_mat.write().await, &msg, lang)).await;
+            Box::pin(disp.message(DEFAULT_VSCROLLD, &mut led_mat, &msg, &lang)).await;
             time_vscroll = Some(true);
         }
     }
 }
 
 async fn reset_button<'a, 'b>(
-    state: Arc<std::pin::Pin<Box<MyState>>>,
+    state: &mut Arc<std::pin::Pin<Box<MyState>>>,
     button: &PinDriver<'a, AnyInputPin, Input>,
-    led_mat: Rc<RwLock<MAX7219<SpiConnector<SpiDeviceDriver<'b, SpiDriver<'b>>>>>>,
+    led_mat: &mut MAX7219<SpiConnector<SpiDeviceDriver<'b, SpiDriver<'b>>>>,
 ) -> anyhow::Result<()> {
     let mut reset_cnt = CONFIG_RESET_COUNT;
     let mut disp = MyDisplay::new_upside_down();
@@ -257,13 +216,13 @@ async fn reset_button<'a, 'b>(
         let msg = format!("Reset? {reset_cnt}");
         error!("{msg}");
         disp.print(&msg, false);
-        disp.show(&mut *led_mat.write().await);
+        disp.show(led_mat);
 
         if reset_cnt == 0 {
             // okay do factory reset now
             error!("Factory resetting...");
             disp.print("Reset...", false);
-            disp.show(&mut *led_mat.write().await);
+            disp.show(led_mat);
 
             let new_config = MyConfig::default();
             new_config.to_nvs(&mut *state.nvs.write().await)?;
